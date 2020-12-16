9af26307bbe2405b86eb3792f551e6e4ef930b43
钟乘永 <zhongcy93@gmail.com>
2020-12-16 08:02:47 +0800

    Add parser

---
+++ lox/parser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kode.zhong on 2020/11/29.
+//
+
+#include "parser.h"
---
+++ lox/parser.h
@@ -0,0 +1,15 @@
+//
+// Created by kode.zhong on 2020/11/29.
+//
+
+#ifndef LOX_INTERPRETER_LOX_PARSER_H_
+#define LOX_INTERPRETER_LOX_PARSER_H_
+#include <vector>
+
+class Parser {
+  void foo(){
+    std::vector<int> dst(12, 1);
+  }
+};
+
+#endif //LOX_INTERPRETER_LOX_PARSER_H_
