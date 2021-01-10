ea35e1ce1b5860f93ad418aa90bafe8e510a194f
钟乘永 <zhongcy93@gmail.com>
2021-01-10 09:05:25 +0800

    modify cmake version

---
+++ lox/interpreter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kode.zhong on 2021/1/10.
+//
+
+#include "interpreter.h"
---
+++ lox/interpreter.h
@@ -0,0 +1,12 @@
+//
+// Created by kode.zhong on 2021/1/10.
+//
+
+#ifndef LOX_INTERPRETER_LOX_INTERPRETER_H_
+#define LOX_INTERPRETER_LOX_INTERPRETER_H_
+
+class interpreter {
+
+};
+
+#endif //LOX_INTERPRETER_LOX_INTERPRETER_H_
